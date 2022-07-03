df04658cc958eaffc83a671e2c6c0c8dc49cfdbc
Yuki Ishida <yuki.ishida@sumida-labs.com>
2022-07-03 11:23:56 +0000

    minor fix

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if ! ls shirasagi; then
-    git clone -b stable https://github.com/shirasagi/shirasagi -b v1.16.1
+    git clone https://github.com/shirasagi/shirasagi -b v1.16.1
     cd shirasagi
     cp -n config/samples/*.{rb,yml} config/
     sed -i -e 's/localhost/mongodb/' config/mongoid.yml
