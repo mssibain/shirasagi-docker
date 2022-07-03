53cbdd475cd27e25110b8651dd1df8657f1199fb
Yuki Ishida <yuki.ishida@sumida-labs.com>
2022-07-03 09:59:07 +0000

    Update ruby to 3.0.4

--- shirasagi_image/run.sh
+++ shirasagi_image/run.sh
@@ -7,13 +7,12 @@ bundle config set --local without 'development test'
 bundle config set --local path 'vendor/bundle'
 bundle install
 
-# Launch
-rake unicorn:start
-
 # なぜか、rakeのバージョンが古い。。とりあえずgemで最新入れてもう一度bundle installしたら大丈夫
 # ここでダメな理由はいつか調べる
 gem install rake
 bundle install
+
+# Launch
 bundle exec rake unicorn:start
 
 # Do not terminate
